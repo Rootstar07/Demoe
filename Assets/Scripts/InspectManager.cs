f8538e457dfc75f040e8c6ea6b0f7b90a1e500d9
GeunTae KIM <k031900@gmail.com>
2021-08-16 20:04:52 +0900

    new Start

---
+++ Assets/Scripts/InspectManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectManager : MonoBehaviour
+{
+    public float distance;
+    public Transform playerSocket;
+
+}
